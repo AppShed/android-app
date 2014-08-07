36abe84d11afcef68d3b510b0a5040038081ca79
Anton Maniskevich <fenixhomepro@gmail.com>
2014-08-07 11:03:29 +0300

    Added add/delete shortcuts

---
+++ app/src/main/java/com/appshed/appstore/LaunchActivity.java
@@ -0,0 +1,21 @@
+package com.appshed.appstore;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by Anton Maniskevich on 8/7/14.
+ */
+public class LaunchActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_launch);
+		if (getIntent().getExtras() != null) {
+			String text = getIntent().getExtras().getString(String.class.getSimpleName());
+			((TextView)findViewById(R.id.txt_text)).setText(text);
+		}
+	}
+}
--- app/src/main/java/com/appshed/appstore/MyActivity.java
+++ app/src/main/java/com/appshed/appstore/MyActivity.java
@@ -18,6 +18,8 @@ public class MyActivity extends Activity implements View.OnClickListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_my);
 		findViewById(R.id.btn_start).setOnClickListener(this);
+		findViewById(R.id.btn_add_shortcut).setOnClickListener(this);
+		findViewById(R.id.btn_delete_shortcut).setOnClickListener(this);
 	}
 
 	@Override
@@ -26,6 +28,46 @@ public class MyActivity extends Activity implements View.OnClickListener {
 			case R.id.btn_start:
 				startActivity(new Intent(MyActivity.this, PhonegapActivity.class));
 				break;
+			case R.id.btn_add_shortcut:
+				addShortcut();
+				break;
+			case R.id.btn_delete_shortcut:
+				removeShortcut();
+				break;
 		}
+	}
+
+	private void addShortcut() {
+		//Adding shortcut for MainActivity
+		//on Home screen
+		Intent shortcutIntent = new Intent(getApplicationContext(), LaunchActivity.class);
+		shortcutIntent.putExtra(String.class.getSimpleName(), "launcher 1");
+
+		shortcutIntent.setAction(Intent.ACTION_MAIN);
+
+		Intent addIntent = new Intent();
+		addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+		addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, "launcher 1");
+		addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
+				Intent.ShortcutIconResource.fromContext(getApplicationContext(),
+						R.drawable.ic_launcher));
+
+		addIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+		getApplicationContext().sendBroadcast(addIntent);
+	}
+
+	private void removeShortcut() {
+
+		//Deleting shortcut for MainActivity
+		//on Home screen
+		Intent shortcutIntent = new Intent(getApplicationContext(),LaunchActivity.class);
+		shortcutIntent.putExtra(String.class.getSimpleName(), "launcher 1");
+		shortcutIntent.setAction(Intent.ACTION_MAIN);
+
+		Intent addIntent = new Intent();
+		addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+		addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, "launcher 1");
+		addIntent.setAction("com.android.launcher.action.UNINSTALL_SHORTCUT");
+		getApplicationContext().sendBroadcast(addIntent);
 	}
 }
