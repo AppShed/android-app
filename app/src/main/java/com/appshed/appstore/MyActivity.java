2adb9903c66b26cf63b2ba2c270e821ca58537d8
Anton Maniskevich <fenixhomepro@gmail.com>
2014-08-07 10:35:45 +0300

    Added camera support plugin

--- app/src/main/java/com/appshed/appstore/MyActivity.java
+++ app/src/main/java/com/appshed/appstore/MyActivity.java
@@ -1,22 +1,31 @@
 package com.appshed.appstore;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import org.apache.cordova.Config;
 import org.apache.cordova.DroidGap;
 
 
-public class MyActivity extends DroidGap {
+public class MyActivity extends Activity implements View.OnClickListener {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_my);
-		super.loadUrl(Config.getStartUrl());
-		super.loadUrl("file:///android_asset/www/index.html");
+		findViewById(R.id.btn_start).setOnClickListener(this);
 	}
 
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.btn_start:
+				startActivity(new Intent(MyActivity.this, PhonegapActivity.class));
+				break;
+		}
+	}
 }
---
+++ app/src/main/java/com/appshed/appstore/PhonegapActivity.java
@@ -0,0 +1,19 @@
+package com.appshed.appstore;
+
+import android.os.Bundle;
+
+import org.apache.cordova.Config;
+import org.apache.cordova.DroidGap;
+
+/**
+ * Created by Anton Maniskevich on 8/7/14.
+ */
+public class PhonegapActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl(Config.getStartUrl());
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+}
