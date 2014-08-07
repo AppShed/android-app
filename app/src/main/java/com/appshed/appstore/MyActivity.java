93b1167261b0e40b6c44fdc47bf6cc3da15147dd
Anton Maniskevich <fenixhomepro@gmail.com>
2014-08-07 10:01:55 +0300

    Added cordova 3.0

--- app/src/main/java/com/appshed/appstore/MyActivity.java
+++ app/src/main/java/com/appshed/appstore/MyActivity.java
@@ -5,32 +5,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-public class MyActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
-    }
+import org.apache.cordova.Config;
+import org.apache.cordova.DroidGap;
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
+public class MyActivity extends DroidGap {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_my);
+		super.loadUrl(Config.getStartUrl());
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+
 }
