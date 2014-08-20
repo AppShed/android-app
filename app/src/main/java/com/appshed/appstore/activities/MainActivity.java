de67031ef7a8cfb59634166223013ac05d858835
Anton Maniskevich <fenixhomepro@gmail.com>
2014-08-20 16:59:07 +0300

    Added menu and page indicator

---
+++ app/src/main/java/com/appshed/appstore/activities/MainActivity.java
@@ -0,0 +1,70 @@
+package com.appshed.appstore.activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.View;
+
+import com.appshed.appstore.R;
+import com.appshed.appstore.fragments.AppGalleryFragment;
+import com.appshed.appstore.fragments.PlaceholderFragment;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.rightutils.rightutils.activities.RightFragmentActivityNew;
+
+/**
+ * Created by Anton Maniskevich on 8/20/14.
+ */
+public class MainActivity extends RightFragmentActivityNew implements View.OnClickListener {
+
+	private SlidingMenu menu;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		initActivity(R.id.fragment_container, new AppGalleryFragment());
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		findViewById(R.id.fragment_container).setOnClickListener(this);
+		setUpMenu();
+	}
+
+	public void setUpMenu() {
+		menu = new SlidingMenu(this);
+		menu.setMode(SlidingMenu.LEFT);
+//		menu.setShadowWidthRes(R.dimen.shadow_width);
+		menu.setShadowDrawable(android.R.color.transparent);
+		menu.setBehindOffset(countWidth(context));
+		menu.setFadeDegree(0.35f);
+//		menu.setShadowDrawable(R.drawable.menu_shadow);
+		menu.attachToActivity(this, SlidingMenu.SLIDING_WINDOW);
+		menu.setMenu(R.layout.sliding_menu);
+		menu.setSlidingEnabled(true);
+		menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+	}
+
+	private static int countWidth(Context context) {
+		DisplayMetrics metrics = new DisplayMetrics();
+		((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(metrics);
+		Display display = ((Activity) context).getWindowManager().getDefaultDisplay();
+		Point size = new Point();
+		display.getSize(size);
+		return size.x / 4;
+	}
+
+	@Override
+	public void onBackPressed() {
+		if (menu.isMenuShowing()) {
+			menu.toggle();
+		} else {
+			super.onBackPressed();
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		menu.toggle();
+	}
+}
---
+++ app/src/main/java/com/appshed/appstore/fragments/AppGalleryFragment.java
@@ -0,0 +1,52 @@
+package com.appshed.appstore.fragments;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.appshed.appstore.R;
+import com.viewpagerindicator.UnderlinePageIndicator;
+
+/**
+ * Created by Anton Maniskevich on 8/20/14.
+ */
+public class AppGalleryFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_app_gallery, null);
+		FragmentPagerAdapter adapter = new GoogleMusicAdapter(getFragmentManager());
+
+		ViewPager pager = (ViewPager) view.findViewById(R.id.pager);
+		pager.setAdapter(adapter);
+
+		UnderlinePageIndicator indicator = (UnderlinePageIndicator)view.findViewById(R.id.indicator);
+		indicator.setViewPager(pager);
+		indicator.setSelectedColor(0xFFFFFF00);
+		indicator.setBackgroundColor(0xFF305EDA);
+		indicator.setMinimumHeight(20);
+		indicator.setFades(false);
+		return view;
+	}
+
+	class GoogleMusicAdapter extends FragmentPagerAdapter {
+		public GoogleMusicAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			return new PlaceholderFragment();
+		}
+
+		@Override
+		public int getCount() {
+			return 3;
+		}
+	}
+}
