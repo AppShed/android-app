352ec11a5cdb9941a7bb1ada9005ca11ff833c67
Anton Maniskevich <fenixhomepro@gmail.com>
2014-09-02 10:23:54 +0300

    Added speener for splash activity

--- app/src/main/java/com/appshed/appstore/activities/SplashScreen.java
+++ app/src/main/java/com/appshed/appstore/activities/SplashScreen.java
@@ -1,10 +1,11 @@
 package com.appshed.appstore.activities;
 
 import android.content.Intent;
-import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
 import com.appshed.appstore.R;
+import com.appshed.appstore.utils.CyclicTransitionDrawable;
 import com.rightutils.rightutils.activities.RightSplashActivity;
 
 /**
@@ -14,17 +15,32 @@ public class SplashScreen extends RightSplashActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		setSplashDelay(15000);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
 		final ImageView animImageView = (ImageView) findViewById(R.id.myImageView);
-		animImageView.setBackgroundResource(R.drawable.progress_bar_list);
-		animImageView.post(new Runnable() {
-			@Override
-			public void run() {
-				AnimationDrawable frameAnimation = (AnimationDrawable) animImageView.getBackground();
-				frameAnimation.start();
-			}
+		CyclicTransitionDrawable ctd = new CyclicTransitionDrawable(new Drawable[] {
+				getResources().getDrawable(R.drawable.anim_1),
+				getResources().getDrawable(R.drawable.anim_2),
+				getResources().getDrawable(R.drawable.anim_3),
+				getResources().getDrawable(R.drawable.anim_4),
+				getResources().getDrawable(R.drawable.anim_5),
+				getResources().getDrawable(R.drawable.anim_6),
+				getResources().getDrawable(R.drawable.anim_7),
+				getResources().getDrawable(R.drawable.anim_8)
 		});
+		animImageView.setImageDrawable(ctd);
+		ctd.startTransition(150, 0);
+//		animImageView.setBackgroundResource(R.drawable.progress_bar_list);
+//		animImageView.post(new Runnable() {
+//			@Override
+//			public void run() {
+//				AnimationDrawable frameAnimation = (AnimationDrawable) animImageView.getBackground();
+//				frameAnimation.setEnterFadeDuration(100);
+//				frameAnimation.setExitFadeDuration(100);
+//				frameAnimation.start();
+//			}
+//		});
 	}
 
 	@Override
