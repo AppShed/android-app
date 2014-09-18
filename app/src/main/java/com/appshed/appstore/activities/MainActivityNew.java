33d3b6f36bbf60168e471d82053cde5905acaac2
Anton Maniskevich <fenixhomepro@gmail.com>
2014-09-18 17:38:29 +0300

    Move cennter in apps list

--- app/src/main/java/com/appshed/appstore/activities/MainActivityNew.java
+++ app/src/main/java/com/appshed/appstore/activities/MainActivityNew.java
@@ -44,43 +44,6 @@ public class MainActivityNew extends SupportRightFragmentActivity implements Vie
 		menu.findViewById(R.id.txt_created_by_me).setOnClickListener(this);
 		menu.findViewById(R.id.txt_app_creator).setOnClickListener(this);
 		menu.findViewById(R.id.qr_scanner_container).setOnClickListener(this);
-
-//		menu = new SlidingMenu(this);
-//		menu.setMode(SlidingMenu.LEFT);
-////		menu.setShadowWidth(-200);
-//		menu.setShadowDrawable(android.R.color.transparent);
-//		menu.setBehindOffset(countWidth(context));
-//		menu.setFadeEnabled(false);
-//		menu.attachToActivity(this, SlidingMenu.SLIDING_WINDOW);
-//		menu.setMenu(R.layout.sliding_menu);
-//		menu.setSlidingEnabled(true);
-//		menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
-//		menu.getMenu().findViewById(R.id.txt_appshed_gallery).setOnClickListener(this);
-//		menu.getMenu().findViewById(R.id.txt_my_saved_apps).setOnClickListener(this);
-//		menu.getMenu().findViewById(R.id.txt_created_by_me).setOnClickListener(this);
-//		menu.getMenu().findViewById(R.id.txt_app_creator).setOnClickListener(this);
-//		menu.getMenu().findViewById(R.id.qr_scanner_container).setOnClickListener(this);
-//		menu.setOnOpenListener(new SlidingMenu.OnOpenListener() {
-//			@Override
-//			public void onOpen() {
-//				menuContentOverlay.setVisibility(View.VISIBLE);
-//			}
-//		});
-//		menu.setOnCloseListener(new SlidingMenu.OnCloseListener() {
-//			@Override
-//			public void onClose() {
-//				menuContentOverlay.setVisibility(View.GONE);
-//			}
-//		});
-	}
-
-	private static int countWidth(Context context) {
-		DisplayMetrics metrics = new DisplayMetrics();
-		((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(metrics);
-		Display display = ((Activity) context).getWindowManager().getDefaultDisplay();
-		Point size = new Point();
-		display.getSize(size);
-		return size.x / 5;
 	}
 
 	@Override
