ce3fa19def1db2c5b4224fc67fd00af9ece8b07e
Anton Maniskevich <fenixhomepro@gmail.com>
2014-09-26 11:14:26 +0300

    Added splash

--- app/src/main/java/com/appshed/store/activities/PhonegapActivity.java
+++ app/src/main/java/com/appshed/store/activities/PhonegapActivity.java
@@ -3,11 +3,13 @@ package com.appshed.store.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
 
 import com.appshed.store.R;
 import com.appshed.store.applications.AppStoreApplication;
@@ -30,17 +32,24 @@ import java.util.concurrent.Executors;
 public class PhonegapActivity extends Activity implements CordovaInterface {
 
 	private static final String TAG = PhonegapActivity.class.getSimpleName();
+	public static final int SPLASH_DELAY = 3000;
 	private CordovaWebView cordovaWebView;
 	protected CordovaPlugin activityResultCallback = null;
 	protected boolean activityResultKeepRunning;
 	protected boolean keepRunning = true;
 	private final ExecutorService threadPool = Executors.newCachedThreadPool();
 	private App selectedApp;
+	private View splashContainer;
+	private ImageView splashImage;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_phonegap);
+		final long startTime = System.currentTimeMillis();
+		splashContainer = findViewById(R.id.splash_container);
+		splashContainer.setOnClickListener(null);
+		splashImage = (ImageView) findViewById(R.id.img_splash);
 		String url = null;
 		if (getIntent().getExtras() != null && getIntent().getExtras().containsKey(App.class.getSimpleName())) {
 			long appId = getIntent().getExtras().getLong(App.class.getSimpleName());
@@ -48,12 +57,26 @@ public class PhonegapActivity extends Activity implements CordovaInterface {
 			selectedApp = AppStoreApplication.dbUtils.getAllWhere(String.format("id = %d", appId), App.class).getFirst();
 			Log.i(TAG, "" + selectedApp);
 			url = "file:" + SystemUtils.getAppFolder(appId) + "/index.html";
+			splashImage.setImageDrawable(BitmapDrawable.createFromPath(SystemUtils.getAppFolder(appId) + "/Defaulth5682x.png"));
 		}
 		cordovaWebView = (CordovaWebView) findViewById(R.id.tutorialView);
 		cordovaWebView.clearCache(true);
 		Config.init(this);
 		cordovaWebView.loadUrl(Config.getStartUrl());
 		cordovaWebView.loadUrl(url);
+		cordovaWebView.setWebViewClient(new WebViewClient(){
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				super.onPageFinished(view, url);
+				long endTime = System.currentTimeMillis();
+				splashContainer.postDelayed(new Runnable() {
+					@Override
+					public void run() {
+						splashContainer.setVisibility(View.GONE);
+					}
+				}, endTime - startTime >= SPLASH_DELAY ? 0 : SPLASH_DELAY-(endTime-startTime));
+			}
+		});
 
 		AdView adView = (AdView) this.findViewById(R.id.adView);
 		if (selectedApp.isAds()) {
